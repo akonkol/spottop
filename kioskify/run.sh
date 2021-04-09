b5d8bdf5d853133b191adc4b571e90a128917416
akonkol <825835+akonkol@users.noreply.github.com>
2021-04-09 21:33:10 +0100

    re-enable kiosk mode

--- kioskify/run.sh
+++ kioskify/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 /usr/bin/chromium-browser -app  http://192.168.1.150/ \
-  --kiosk \
   --noerrdialogs \
   --disable-session-crashed-bubble \
   --disable-infobars \
-  --check-for-update-interval=604800 \
-  --disable-pinch
+  --disable-pinch \
+  --kiosk
