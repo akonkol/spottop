9b975ac07d2f64df9e11055c313ec8bef3e7b1c1
andrew.konkol <andrew.konkol@getbraintree.com>
2021-04-19 15:26:49 -0500

    Run applescript with launchd Get current song from spotify via applescript, parse the output and relay that information to upstream spottop-reporter endpoint as json

--- applescript/get_local_spotify_info.py
+++ applescript/get_local_spotify_info.py
@@ -6,15 +6,16 @@ import sys
 
 def run_applescript():
   result = subprocess.run(
-      ["osascript", "spottop.applescript"], capture_output=True, text=True
+      ["/usr/bin/osascript", "/Users/akonkol/code/spottop/applescript/spottop.applescript"], capture_output=True, text=True
   )
   if result.stderr:
-      return json.dumps(None)
+      return result.stderr
   else:
       return result.stdout
 
 current_track = run_applescript()
 r = requests.post('http://192.168.1.150:8080/json-input', json=json.loads(current_track))
 if r.status_code != 200:
+    print("failed to talk to spotop json-input")
+else:
     pass
-
